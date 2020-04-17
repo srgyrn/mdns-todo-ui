5fb468b18354cb817fc822b12e68f5b488acf7e1
Sera Geyran <sgeyran@gmail.com>
2020-04-17 22:02:22 +0300

    Fixed tests. Deleted router component -no idea why installed it in the first place

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,43 +1,71 @@
 import { mount } from "@vue/test-utils";
 import List from "@/components/List";
-import { getList } from "@/services/axios"
-import flushPromises from 'flush-promises'
+import { getList, postItem } from "@/services/axios";
+import flushPromises from "flush-promises";
 
 jest.mock("@/services/axios");
 
-describe('List items', () => {
-    it('Error message box is invisible by default', () => {
-        const wrapper = mount(List)
-        expect(wrapper.find("#errors").isVisible()).toBe(false)
+describe("List items and API calls", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Error message box is invisible by default", async () => {
+    getList.mockResolvedValueOnce([]);
+    const wrapper = mount(List);
+    expect(wrapper.find("#errors").isVisible()).toBe(false);
+  });
+
+  it("Displays error when empty list item is submitted.", async () => {
+    getList.mockResolvedValueOnce([]);
+
+    const wrapper = mount(List);
+    const input = wrapper.find('[data-testId="item-input"]');
+    const button = wrapper.find('[data-testId="submit-button"]');
+
+    input.setValue(" ");
+    button.trigger("click");
+
+    await wrapper.vm.$nextTick();
+
+    const errorPane = wrapper.find("#errors");
+    expect(errorPane.isVisible()).toBe(true);
+    expect(errorPane.html()).toContain(
+      "Whoopsie! You seem to have forgotten to enter a new item."
+    );
+  });
+
+  it("Calls getList when loaded", async () => {
+    getList.mockResolvedValueOnce([
+      { id: 1, content: "test item", isCompleted: false }
+    ]);
+
+    const wrapper = mount(List);
+
+    await flushPromises();
+    expect(getList).toHaveBeenCalledTimes(1);
+
+    const itemDisplay = wrapper.find('[data-testId="list-display"]');
+    expect(itemDisplay.html()).toContain("test item");
+  });
+
+  it("Adds item", async () => {
+    getList.mockResolvedValueOnce([]);
+    postItem.mockResolvedValueOnce({
+      id: 1,
+      content: "test item",
+      isCompleted: false
     });
+    const wrapper = mount(List);
+    await flushPromises();
+    const input = wrapper.find('input[data-testId="item-input"]');
+    const button = wrapper.find('button[data-testId="submit-button"]');
 
-    it("Displays error when empty list item is submitted.", async () => {
-        const wrapper = mount(List)
-        const input = wrapper.find('[data-testId="item-input"]')
-        const button = wrapper.find('[data-testId="submit-button"]')
+    input.setValue("test item");
+    button.trigger("click");
+    await wrapper.vm.$nextTick();
 
-        input.setValue(" ")
-        button.trigger('click')
-
-        let errorPane = wrapper.find("#errors")
-        expect(errorPane.isVisible()).toBe(true)
-        expect(errorPane.html()).toContain("<p>Whoopsie! You seem to have forgotten to enter a new item.</p>")
-    });
-});
-
-describe("API calls", function () {
-    beforeEach(() => {
-        jest.clearAllMocks()
-    });
-
-    it("Calls getItems and displays the list", async () => {
-        const mockResponse = '{{"item": "test item", "isCompleted": false}}'
-        getList.mockResolvedValueOnce({text: mockResponse})
-        const wrapper = mount(List)
-
-        await flushPromises()
-        const itemDisplay = wrapper.find('[data-testId="list-display"]')
-        expect(itemDisplay.element.childElementCount).toEqual(1)
-        expect(itemDisplay.html()).toContain("test item")
-    });
+    expect(wrapper.find("#errors").isVisible()).toBe(false);
+    expect(postItem).toHaveBeenCalledTimes(1);
+  });
 });
